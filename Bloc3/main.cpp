4379cf68d3f6095487c62482ae59b66056990012
Pinkii <pinkiibiza@gmail.com>
2014-10-28 00:02:10 +0100

    se me ha rotido el glut

--- Bloc3/main.cpp
+++ Bloc3/main.cpp
@@ -6,6 +6,7 @@
 #include <GL/gl.h>
 #include <GL/freeglut.h>
 #include "model/model.h"
+#include <math.h>
 
 #define help std::cout << "|------------------------------------|" << std::endl << "|Aiudame, io tengo muchos quereseres.|" << std::endl << "|Aiudame! porque mi casa esta súsia. |"<< std::endl << "|------------------------------------|" << std::endl << std::endl;
 
@@ -24,6 +25,20 @@ enum Showing {
 
 enum Models {
     homer, legoman, phomer, qttModels
+};
+
+enum CameraType {
+    ortho, perspective, qttCameraType
+};
+
+struct Vector3f {
+    float x, y, z;
+    Vector3f() {}
+    Vector3f(float x,float y, float z) {
+        this->x = x;
+        this->y = y;
+        this->z = z;
+    }
 };
 
 struct Caja {
@@ -57,21 +72,19 @@ struct Caja {
             }
         }
     }
+    Vector3f findCenter() {
+        Vector3f centro;
+        centro.x = ((maxX-minX)/2)+minX;
+        centro.y = ((maxY-minY)/2)+minY;
+        centro.z = ((maxZ-minZ)/2)+minZ;
+        return centro;
+    }
 };
 
-struct Vector3f {
-    float x, y, z;
-    Vector3f() {}
-    Vector3f(float x,float y, float z) {
-        this->x = x;
-        this->y = y;
-        this->z = z;
-    }
-    Vector3f findCenter(Caja b) {
-        x = ((b.maxX-b.minX)/2)+b.minX;
-        y = ((b.maxY-b.minY)/2)+b.minY;
-        z = ((b.maxZ-b.minZ)/2)+b.minZ;
-    }
+
+struct Esfera {
+    Vector3f centro;
+    float radio;
 };
 
 struct Muneco {
@@ -81,18 +94,34 @@ struct Muneco {
     Vector3f centro;
     Vector3f tamano;
     Vector3f posFinal;
+    float scalated;
     Muneco () {}
-    Muneco (Models name) {
+    Muneco (Models name,float s) {
+        std::cout << "Creando nuevo modelo" << std::endl << "Su escalado es " << s << std::endl;
+        scalated = s;
         changeModel(name);
-        posFinal = Vector3f(-0.75,-0.39,0.75);
+        posFinal = Vector3f(-0.75+tamano.x/2.f,-0.39+tamano.y/2.f,0.75-tamano.z/2.f);
+        std::cout << "Pos Final: " << posFinal.x << " " << posFinal.y << " " << posFinal.z << std::endl;
     }
     void changeModel(Models name) {
+        std::cout << "Cambiando el modelo";
         this->name = name;
         if (name == homer) modelo.load(HOMER);
         else if (name == legoman)modelo.load(LEGOMAN);
         else modelo.load(PHOMER);
+        std::cout << "    Modelo cargado" << std::endl;
         box.createBox(modelo);
-        centro.findCenter(box);
+        centro = box.findCenter();
+        size();
+    }
+    void size() {
+        float smax = std::max(box.maxX-box.minX,std::max(box.maxY-box.minY,box.maxZ-box.minZ));
+        float xxx, yyy, zzz;
+        xxx = scalated*(box.maxX-box.minX)/smax;
+        yyy = scalated*(box.maxY-box.minY)/smax;
+        zzz = scalated*(box.maxZ-box.minZ)/smax;
+        tamano = Vector3f(xxx,yyy,zzz);
+        std::cout << "Su tamaño es " << tamano.x << " " << tamano.y << " " << tamano.z << std::endl;
     }
 };
 
@@ -104,12 +133,14 @@ std::pair<bool,bool> mouse;
 float r, g, b, a;
 State state;
 Showing showing;
+CameraType cameraState;
 char* textState;
 char* textShow;
-int rotateX,rotateY;
+int rotateX,rotateY,rotateZ;
+float dist;
 float monigote;
 Muneco m;
-
+Esfera s;
 
 
 void changeState(State s) {
@@ -149,24 +180,62 @@ void changeShowing(Showing s) {
     glutPostRedisplay();
 }
 
-void initCamara() {
+Esfera esferaContenidora() { //hardcodeado a ful
+    Caja aux;
+    aux.maxX = std::max(m.posFinal.x+m.tamano.x/2,0.7f);
+    aux.minX = std::min(m.posFinal.x-m.tamano.x/2,-0.7f);
+    aux.maxY = std::max(m.posFinal.y+m.tamano.y/2,-0.4f);
+    aux.minY = std::min(m.posFinal.y-m.tamano.y/2,-0.4f);
+    aux.maxZ = std::max(m.posFinal.z+m.tamano.z/2,0.7f);
+    aux.minZ = std::min(m.posFinal.z-m.tamano.z/2,-0.7f);
+
+
+    Vector3f centro = aux.findCenter();
+    Esfera ret;
+    ret.radio = pow(pow(aux.maxX-centro.x,2)+pow(aux.maxY-centro.y,2)+pow(aux.maxZ-centro.z,2),1.0/2.0);
+    ret.centro = centro;
+    return ret;
+}
+
+void updateCameraType(CameraType t) {
+    s = esferaContenidora();
+    float r = s.radio;
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(-1,1,-1,1,-1,1);
+    if (t == ortho) {
+        float wMin = std::min(rWidth,rHeight);
+        glOrtho(-r*(rWidth/wMin)/2+s.centro.x,r*(rWidth/wMin)/2+s.centro.x,
+                -r*(rHeight/wMin)/2+s.centro.y,r*(rHeight/wMin)/2+s.centro.y,
+                -r+s.centro.z+dist,r+s.centro.z+dist);
+    }
+    else {
+        float fov = asin(r/dist);
+        gluPerspective(fov,rWidth/rHeight,-r+s.centro.z+dist,r+s.centro.z+dist);
+    }
     glMatrixMode(GL_MODELVIEW);
 }
 
+void setCameraPos() {
+    glLoadIdentity();
+//    gluLookAt();
+}
+
 void init() {
-    initCamara();
-    m = Muneco(legoman);
+    m = Muneco(legoman,0.5);
     state = rotating;
     textState = "HELLO";
     showing = sFill;
     textShow = "Fill";
     r = g = 1;
     b = a = 0.8;
-    monigote = 0.4;
-    rotateX = rotateY = 0;
+    monigote = 0.39;
+    rotateX = rotateY = 45;
+    rotateZ = 0;
+    dist = 0.2;
+
+    cameraState = ortho;
+    updateCameraType(cameraState);
+    setCameraPos();
 }
 
 void myBodyIsReady(float size) {
@@ -194,26 +263,21 @@ void ejes() {
     glBegin(GL_LINES);glColor3f(0.0f, 0.0f, 1.0f);glVertex3f(0,0,0);glVertex3f(0,0,1);glEnd();
 }
 
-void daModelIsReal(Model mm, float size) {
-    float xxx, yyy, zzz;
+void daModelIsReal(Muneco m) {
     float smax = std::max(m.box.maxX-m.box.minX,std::max(m.box.maxY-m.box.minY,m.box.maxZ-m.box.minZ));
-    xxx = size*(m.box.maxX-m.box.minX)/smax;
-    yyy = size*(m.box.maxY-m.box.minY)/smax;
-    zzz = size*(m.box.maxZ-m.box.minZ)/smax;
-
-    m.tamano = Vector3f(xxx,yyy,zzz);
-    glScaled(xxx/(m.box.maxX-m.box.minX),yyy/(m.box.maxY-m.box.minY),zzz/(m.box.maxZ-m.box.minZ));
+    glScaled(m.scalated/smax,m.scalated/smax,m.scalated/smax);
     glTranslated(-m.centro.x, -m.centro.y, -m.centro.z);
+
 
     glBegin(GL_TRIANGLES);
 
-    for (int i = 0; i < mm.faces().size(); ++i) {
+    for (int i = 0; i < m.modelo.faces().size(); ++i) {
 
-        const Face &f = mm.faces()[i];
+        const Face &f = m.modelo.faces()[i];
         glColor4fv(Materials[f.mat].diffuse);
         const Vertex *v;
         for (int j = 0; j < 3; ++j) {
-            v = &mm.vertices()[f.v[j]];
+            v = &m.modelo.vertices()[f.v[j]];
             glVertex3dv(v);
         }
     }
@@ -249,9 +313,19 @@ void drawGround(Vector3f centro, Vector3f plano, float size) {
 void refresh(void) {
     glClearColor(r,g,b,a);
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
 glPushMatrix();
+
+
+    glTranslated(0.,0.,-dist);
+    glRotated(rotateZ,0,0,1);
     glRotated(rotateY,1,0,0);
     glRotated(rotateX,0,1,0);
+    glTranslated(-s.centro.x,-s.centro.y,-s.centro.z);
+
+    glColor3f(0,0,0);
+    glutWireSphere(s.radio,20,20);
+//    glTranslated(-s.centro.x,-s.centro.y,-s.centro.z);
     glPushMatrix();
         glColor3f(1,1,0);
         drawGround(Vector3f(0,-0.4,0), Vector3f(1,0,1), 1.4);
@@ -264,8 +338,8 @@ glPushMatrix();
         myBodyIsReady(monigote);
     glPopMatrix();
     glPushMatrix();
-        glTranslated(m.posFinal.x+m.tamano.x/2.f,m.posFinal.y+m.tamano.y/2.f,m.posFinal.z-m.tamano.z/2.f);
-        daModelIsReal(m.modelo, 0.5);
+        glTranslated(m.posFinal.x,m.posFinal.y,m.posFinal.z);
+        daModelIsReal(m);
     glPopMatrix();
     glPopMatrix();
     glPushMatrix();
@@ -285,15 +359,8 @@ glPushMatrix();
 void resize(int x, int y) {
     rWidth = x;
     rHeight = y;
-
-    float nX, nY;
-    if (rWidth > rHeight) portrait= rHeight;
-    else portrait = rWidth;
-
-    nX = abs(rWidth-portrait)/2.f;
-    nY = abs(rHeight-portrait)/2.f;
-
-    glViewport(nX, nY, portrait, portrait);
+    glViewport(0,0,x,y);
+    updateCameraType(cameraState);
 }
 
 void mouseClicked(int button, int pressed, int x, int y) {
@@ -320,6 +387,7 @@ void mouseMoved(int x, int y) {
 
         posMouse.first = x;
         posMouse.second = y;
+        updateCameraType(cameraState);
         glutPostRedisplay();
     }
 }
@@ -343,9 +411,7 @@ void teclado(unsigned char c, int x, int y) {
         else if (state == moving) changeState(rotating);
         break;
     case 'c':
-        if (state == moving) changeState(rotating);
-        else if (state == rotating) changeState(moving);
-        break;
+        cameraState = (cameraState+1)%qttCameraType;
     case 's':
         changeShowing((showing+1)%qttShowing);
         break;
@@ -354,11 +420,11 @@ void teclado(unsigned char c, int x, int y) {
         glutPostRedisplay();
         break;
     case 'z':
-        monigote += 0.01;
+        dist += 0.01;
         glutPostRedisplay();
         break;
     case 'x':
-        monigote -= 0.01;
+        dist -= 0.01;
         glutPostRedisplay();
         break;
     default:
